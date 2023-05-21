5b66214b65778a2f36b0f7bcbdfcbb596aa1cc15
arayl07 <133981833+arayl07@users.noreply.github.com>
2023-05-22 01:57:02 +0600

    18

---
+++ may19.py
@@ -0,0 +1,53 @@
+#первая задача
+user_input = input("введите число: ")
+user_int = int(user_input)
+
+result = user_int **2
+print("Результат:", result)
+
+#вторая задача
+print('введите 2 числа')
+a = int(input(10))
+b = int(input(10))
+c = (a+b)/2
+print("результат:", result)
+#почему ответ 10?
+
+#третяя задача
+user_input = input("площадь квадрата: ")
+user_int = int(user_input)
+result = (a*b)
+print("площадь квадрата:" , result)
+
+user_input = input("вывести ариф: ")
+user_int1 = int(user_input)
+user_input = input("вывести ариф: ")
+user_int2 = int(user_input)
+result= (user_int1 + user_int2) / 2
+print("результат:", result)
+
+#четвертая задача
+km=float(input('km='))
+print('am=',km*0.6214)
+
+#task6
+user_input = input("введите значение для формулы: ")
+if a == 0 and b != 0:
+print("корней нет")
+
+elif a == 0 and b == 0:
+print("x - любое число")
+else: 
+if b == 0 and a != 0:
+else:
+x = -b/a
+print("корень уравнения", x) 
+
+
+#task7
+a=int(input("введите кол-во часов данный момент))
+b=int(input("введите кол-во минут в данный момент))
+c=24-a
+z=60*24
+d=z-b
+print (c, d)

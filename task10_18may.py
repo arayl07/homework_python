833f98c26ee2fa9064162001453ddc3065c29ecf
arayl07 <133981833+arayl07@users.noreply.github.com>
2023-05-28 22:28:04 +0600

    задача 10 от 18 мая
    Посчитать зарплату

---
+++ task10_18may.py
@@ -0,0 +1,11 @@
+obchaya_prodaja = int(input("Введите общую сумму продаж за месяц: $"))
+
+oklad = 250
+procent_prodaji = 0.1
+
+
+komisiya = obchaya_prodaja * procent_prodaji
+zarplata = oklad + komisiya
+
+print("Зарплата работника составляет: $")
+
